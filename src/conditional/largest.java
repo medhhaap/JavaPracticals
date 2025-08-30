e8a2668a72c746079e10229f0f4ac0acaea5802b
medhhaap <medhhaap@icloud.com>
2025-08-31 04:02:49 +0545

    new commit

--- src/conditional/largest.java
+++ src/conditional/largest.java
@@ -1,10 +1,8 @@
 package conditional;
-
 import java.util.Scanner;
 
 public class largest {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
@@ -16,11 +14,14 @@ public class largest {
         System.out.print("Enter third number: ");
         int num3 = scanner.nextInt();
 
+        // if-else-if structure for 3 numbers
         if (num1 >= num2 && num1 >= num3) {
             System.out.println(num1 + " is the largest.");
-        } else if (num2 >= num1 && num2 >= num3) {
+        }
+        else if (num2 >= num1 && num2 >= num3) {
             System.out.println(num2 + " is the largest.");
-        } else if (num3 >= num1 && num3 >= num2) {
+        }
+        else if (num3 >= num1 && num3 >= num2) {
             System.out.println(num3 + " is the largest.");
         }
 
