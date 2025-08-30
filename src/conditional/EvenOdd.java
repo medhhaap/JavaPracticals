ebfba53e47ae50603d55e6c9d62ebced56e4b7d5
medhhaap <medhhaap@icloud.com>
2025-08-30 23:54:53 +0545

    new commit

---
+++ src/conditional/EvenOdd.java
@@ -0,0 +1,18 @@
+package conditional;
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Create Scanner object
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt(); // Read input
+
+        // Check even or odd
+        if (num % 2 == 0)
+            System.out.println(num + " is Even.");
+        else
+            System.out.println(num + " is Odd.");
+
+        sc.close(); // Close Scanner (inside main)
+    }
+}
