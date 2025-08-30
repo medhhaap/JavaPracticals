5bf2457276c23e42b863c30a3d059b5a49302af5
medhhaap <medhhaap@icloud.com>
2025-08-31 00:05:45 +0545

    new commit

---
+++ src/conditional/largest.java
@@ -0,0 +1,35 @@
+package conditional;
+
+import java.util.Scanner;
+
+public class largest {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+
+        System.out.print("Enter fourth number: ");
+        int num4 = scanner.nextInt();
+
+        // Using if-else-if for 4 numbers
+        if (num1 >= num2 && num1 >= num3 && num1 >= num4) {
+            System.out.println(num1 + " is the largest.");
+        } else if (num2 >= num1 && num2 >= num3 && num2 >= num4) {
+            System.out.println(num2 + " is the largest.");
+        } else if (num3 >= num1 && num3 >= num2 && num3 >= num4) {
+            System.out.println(num3 + " is the largest.");
+        } else {
+            System.out.println(num4 + " is the largest.");
+        }
+
+        scanner.close();
+    }
+}
